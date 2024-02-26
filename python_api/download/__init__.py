f57b4651a7e4ec0f06e376734b2aab4e410d129f
fynn.trautvetter <fynn.trautvetter@accenture.com>
2024-02-26 16:43:01 +0100

    delete smthng

--- python_api/download/__init__.py
+++ python_api/download/__init__.py
@@ -7,13 +7,6 @@ from new_class_24 import ApiClient
 
 
 
-tenant_id = "705d07a3-2eea-4f3b-ab59-65ca29abeb26"
-client_id = "7bd4c8a5-fddf-4c44-9042-7b77aff69932"
-client_secret = "U5z8Q~Qi3jt3eGPgIVu4I5EBLYHG6Yt6IfnLBaPx&scope=api%3A%2F%2F8b75aa22-29f8-459b-a52b-6b23fb653003%2F.default"
-base_url='https://webapp-msfwex23-30-gpt-api-qa.azurewebsites.net'
-
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     client=ApiClient(tenant_id, client_id, client_secret, base_url)
     try:
--- python_api/search/__init__.py
+++ python_api/search/__init__.py
@@ -7,10 +7,10 @@ from new_class_24 import ApiClient
 from helpers import transform_document_data_search
 
 
-tenant_id = "705d07a3-2eea-4f3b-ab59-65ca29abeb26"
-client_id = "7bd4c8a5-fddf-4c44-9042-7b77aff69932"
-client_secret = "U5z8Q~Qi3jt3eGPgIVu4I5EBLYHG6Yt6IfnLBaPx&scope=api%3A%2F%2F8b75aa22-29f8-459b-a52b-6b23fb653003%2F.default"
-base_url='https://webapp-msfwex23-30-gpt-api-qa.azurewebsites.net'
+tenant_id =
+client_id =
+client_secret =
+base_url=
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     client=ApiClient(tenant_id, client_id, client_secret, base_url)
